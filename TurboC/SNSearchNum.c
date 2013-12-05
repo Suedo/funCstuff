0eb70b1e8618146064e5ad23a15fedab4a35eef7
Suedo <somjitnag192@gmail.com>
2013-12-05 23:02:38 +0530

    TurboC code with gcc

--- TurboC/SNSearchNum.c
+++ TurboC/SNSearchNum.c
@@ -3,24 +3,38 @@
 */
 #include <stdio.h>
 #include <conio.h>
+#include <stdlib.h>
 
 int main(void){
-	int i;n; int arr[20] , query; 
+
+	int i,n, query; 
+	int* arr;
 	
 	printf("enter number of numbers : ");
 	scanf("%d",&n);
-
-	printf("Enter numbers(max 20): ");
+	arr = (int*) calloc (n,sizeof(int));
+	
+	printf("Enter %d numbers : \n",n);
 	// populate the array
-	for(i=0 ; i < n ; i++) scanf("%d",arr[i]);
+	for(i=0 ; i < n ; i++){
+		printf("> ");scanf("%d",&arr[i]);
+	}
+	printf("\n entered array : ");
+	for(i=0 ; i < n ; i++) printf("%d ",arr[i]);
+	printf("\n");
 
 	printf("enter query : ");
 	scanf("%d",&query);	
 
 	
 	// for small arrays , liner search is sufficient
-	for(i=0;i<n;i++) if(arr[i]==query) { printf("found %d at %d'th location\n",query,i); break; }
-	
+	for(i=0;i<n;i++){
+		if(arr[i]==query) { 
+			printf("found %d at %d'th location\n",query,i); 
+			break;
+		}
+	}
 	getch();
-	clrscr(); return 0;
+	//clrscr(); // works with turbo C only , compiling this at home on gcc
+	return 0;
 }
